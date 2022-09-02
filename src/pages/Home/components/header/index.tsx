300a39f1c2b984ddf84b0b1be7ce39673b1c1b61
Nick <rockzje@gmail.com>
2022-09-02 15:28:23 +0400

    first rxjs task

--- src/pages/Home/components/header/index.tsx
+++ src/pages/Home/components/header/index.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles.module.css';
-import { combineLatest, timer, Observable, map, fromEvent } from 'rxjs';
-import { $img } from 'react-rxjs-elements';
-import { firstImg, secondImg, thirdImg } from './images';
+import { combineLatest, Observable } from 'rxjs';
+
+function loadImage(url: string): Observable<string> {
+  return new Observable(function subscriber(subscriber) {
+    let img = new Image();
+    img.onload = function onload() {
+      subscriber.next(url);
+      subscriber.complete();
+    };
+    img.onerror = function onerror(err) {
+      subscriber.error(err);
+    };
+    img.src = url;
+    img.remove();
+  });
+}
 
 export default function Header() {
-  const src$: any = '';
+  const [images, setImages] = useState<string[]>([]);
   const imgOne = 'https://www.dating.com/i/index/logo.svg?2';
   const imgTwo =
     'https://help.twitter.com/content/dam/help-twitter/brand/logo.png';
+
   const imgThree =
     'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png';
 
-  combineLatest();
+  const imageOneLoad = loadImage(imgOne);
+  const imageTwoLoad = loadImage(imgTwo);
 
-  // const src$: any = timer(0, 1000).pipe(
-  //   map((x) => (x % 2 ? firstImg : secondImg))
-  // );
-  // src$.subscribe();
+  const imageThreeLoad = loadImage(imgThree);
+  useEffect(() => {
+    combineLatest([imageOneLoad, imageTwoLoad, imageThreeLoad]).subscribe(
+      (arr) => {
+        console.log(arr);
+        setImages(arr);
+      }
+    );
+  }, []);
+
   return (
     <div>
-      <$img className={styles.headerLogo} src={src$} alt="logo" />
+      {images.map((src) => (
+        <img src={src} key={src} className={styles.headerLogo} />
+      ))}
     </div>
   );
 }
