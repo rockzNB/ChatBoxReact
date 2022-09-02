7b4287237446903ae92b76392a44ed564cc99207
Nick <rockzje@gmail.com>
2022-09-02 13:26:33 +0400

    testing

---
+++ src/pages/Home/components/header/images.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const firstImg = 'https://www.dating.com/i/index/logo.svg?2';
+export const secondImg =
+  'https://help.twitter.com/content/dam/help-twitter/brand/logo.png';
+export const thirdImg =
+  'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png';
--- src/pages/Home/components/header/index.tsx
+++ src/pages/Home/components/header/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import styles from './styles.module.css';
+import { combineLatest, timer, Observable, map, fromEvent } from 'rxjs';
+import { $img } from 'react-rxjs-elements';
+import { firstImg, secondImg, thirdImg } from './images';
 
 export default function Header() {
+  const src$: any = '';
+  const imgOne = 'https://www.dating.com/i/index/logo.svg?2';
+  const imgTwo =
+    'https://help.twitter.com/content/dam/help-twitter/brand/logo.png';
+  const imgThree =
+    'https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587515358_10512.png';
+
+  combineLatest();
+
+  // const src$: any = timer(0, 1000).pipe(
+  //   map((x) => (x % 2 ? firstImg : secondImg))
+  // );
+  // src$.subscribe();
   return (
     <div>
-      <img
-        className={styles.headerLogo}
-        src="https://www.dating.com/i/index/logo.svg?2"
-        alt="logo"
-      />
+      <$img className={styles.headerLogo} src={src$} alt="logo" />
     </div>
   );
 }
